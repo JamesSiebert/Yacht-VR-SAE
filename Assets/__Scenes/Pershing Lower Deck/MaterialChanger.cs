a0a6117d174faec95989a044c2d4996b2baa38b4
James Siebert <js@media360.com.au>
2021-08-03 11:02:11 +1000

    interactions and material changer in lower deck

--- Assets/__Scenes/Pershing Lower Deck/MaterialChanger.cs
+++ Assets/__Scenes/Pershing Lower Deck/MaterialChanger.cs
@@ -7,10 +7,11 @@ public class MaterialChanger : MonoBehaviour
 {
     public GameObject[] woodObjects;
     public Material[] woodMaterials;
+    public int woodCurrentId;
     
     public GameObject[] bedSheetObjects;
     public Material[] bedSheetMaterials;
-
+    public int bedSheetCurrentId;
 
     private void Start()
     {
@@ -18,21 +19,43 @@ public class MaterialChanger : MonoBehaviour
         bedSheetObjects = GameObject.FindGameObjectsWithTag("BedSheet");
     }
 
+    
     // Change wood materials
     public void ChangeWood(int materialId)
     {
+        woodCurrentId = materialId;
         ChangeMaterial(materialId, woodMaterials, woodObjects);
     }
+    
+    public void ChangeWoodNext()
+    {
+        woodCurrentId += 1;
+        if (woodCurrentId > woodMaterials.Length - 1)
+            woodCurrentId = 0;
+        ChangeMaterial(woodCurrentId, woodMaterials, woodObjects);
+    }
+    
     
     // Change bed sheet materials
     public void ChangeBedSheets(int materialId)
     {
+        bedSheetCurrentId = materialId;
         ChangeMaterial(materialId, bedSheetMaterials, bedSheetObjects);
     }
+    
+    public void ChangeBedSheetNext()
+    {
+        bedSheetCurrentId += 1;
+        if (bedSheetCurrentId > bedSheetMaterials.Length - 1)
+            bedSheetCurrentId = 0;
+        ChangeMaterial(bedSheetCurrentId, bedSheetMaterials, bedSheetObjects);
+    }
+    
     
     // Change all array object's materials
     public void ChangeMaterial(int materialId, Material[] materials, GameObject[] items)
     {
+        Debug.Log("Material Change");
         foreach (var item in items)
         {
             item.GetComponent<MeshRenderer> ().material = materials[materialId];
